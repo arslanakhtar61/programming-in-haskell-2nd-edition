21f468a919b835ae4abe1bfa46cc258fc1f4a01e
Adrian Wong <adrianwong@Adrians-MacBook-Air.local>
2016-11-09 13:18:11 +1100

    Solutions to Chapter 01

---
+++ ch01_solutions.hs
@@ -0,0 +1,15 @@
+-- Q4
+-- Swap occurrences of 'smaller' and 'larger'
+qsort []     = []
+qsort (x:xs) = qsort larger ++ [x] ++ qsort smaller
+               where
+                   smaller = [a | a <- xs, a <= x]
+                   larger  = [b | b <- xs, b > x]
+
+-- Q5
+-- Duplicates will be removed from the sorted list
+qsort' []     = []
+qsort' (x:xs) = qsort' smaller ++ [x] ++ qsort' larger
+                where
+                    smaller = [a | a <- xs, a < x]
+                    larger  = [b | b <- xs, b > x]
