afd0f6e8f30a84d9bff8526bac264455a99b6314
Adrian Wong <adrianwjw@gmail.com>
2016-11-09 19:35:10 +1100

    Solutions to Chapter 04

---
+++ ch04_solutions.hs
@@ -0,0 +1,23 @@
+-- Q5
+func :: Bool -> Bool -> Bool
+func a b = if a then
+               if b then True else False
+           else False
+
+-- Q6
+func' :: Bool -> Bool -> Bool
+func' a b = if a then b else False
+
+-- Q7
+mult :: Int -> Int -> Int -> Int
+mult = \x -> (\y -> (\z -> (x*y*z)))
+
+-- Q8
+luhnDouble :: Int -> Int
+luhnDouble x
+    | n < 10    = n
+    | otherwise = n-9
+    where n = x*2
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn w x y z = ((luhnDouble w) + x + (luhnDouble y) + z) `mod` 10 == 0
