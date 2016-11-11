d972802cd837f26c3788fb7134e0e3887589f9f9
Adrian Wong <adrianwjw@gmail.com>
2016-11-11 22:51:17 +1100

    Solutions to Chapter 07 (incomplete)

--- ch07_solutions.hs
+++ ch07_solutions.hs
@@ -27,3 +27,20 @@ dropWhile' p [] = []
 dropWhile' p (x:xs)
     | p x       = dropWhile' p xs
     | otherwise = x:xs
+
+-- Q3
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> f x : xs) []
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x xs -> if p x then x : xs else xs) []
+
+-- Q4
+{- 1000*2 + 100*3 + 10*4 + 1*5
+   10(100*2 + 10*3 + 1*4) + 1*5
+   10(10(10*2 + 1*3) + 1*4) + 1*5
+   10(10(10(10*0 + 1*2) + 1*3) + 1*4) + 1*5
+   10(10(10(10*0 +  y ) +  y ) +  y ) +  y
+-}
+dec2int :: [Int] -> Int
+dec2int = foldl (\x y -> 10*x + y) 0
