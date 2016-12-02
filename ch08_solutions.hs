89737f4758b0c2f53216dd5936a1557439a54b1d
Adrian Wong <adrianwjw@gmail.com>
2016-12-02 12:52:22 +1100

    Solutions to Chapter 08 (incomplete)

--- ch08_solutions.hs
+++ ch08_solutions.hs
@@ -17,3 +17,23 @@ occurs x (Node l y r) = case compare x y of
                              LT -> occurs x l
                              EQ -> True
                              GT -> occurs x r
+
+-- Q3
+data Tree' a = Leaf' a | Node' (Tree' a) (Tree' a)
+
+numLeaves :: Tree' a -> Int
+numLeaves (Leaf' x)   = 1
+numLeaves (Node' l r) = numLeaves l + numLeaves r
+
+balanced :: Tree' a -> Bool
+balanced (Leaf' x)   = True
+balanced (Node' l r) = abs (numLeaves l - numLeaves r) <= 1 && balanced l && balanced r
+
+-- Q4
+halveList :: [a] -> ([a],[a])
+halveList xs = splitAt (length xs `div` 2) xs
+
+balance :: [a] -> Tree' a
+balance [x] = Leaf' x
+balance xs  = Node' (balance ys) (balance zs)
+              where (ys,zs) = halveList xs
